19a1faa97e14d21ec0f3dc02c53dbf6f0816d07f
Arun Sakthi Anand M <arunsakthianand@gmail.com>
2025-09-22 19:22:17 +0530

    Create CommentDto.java

---
+++ ytclone/src/main/java/com/arun/ytclone/dto/CommentDto.java
@@ -0,0 +1,16 @@
+package com.arun.ytclone.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentDto {
+
+    private String text;
+
+    private String authorID;
+
+}
